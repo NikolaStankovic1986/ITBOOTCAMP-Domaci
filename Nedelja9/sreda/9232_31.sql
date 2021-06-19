2ff830608d7995e032ddd0adc8d014ca3efbec6d
Nikola Stanković <stankovic.nikola@eps.rs>
2021-06-19 20:25:03 +0200

    promenjen je fajl za ponedeljak

--- Nedelja9/sreda/9232_31.sql
+++ Nedelja9/sreda/9232_31.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 19, 2021 at 07:49 PM
+-- Generation Time: Jun 19, 2021 at 08:23 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.5
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `djaci` (
   `id` int(11) NOT NULL,
-  `broj_knjizice` varchar(20) NOT NULL,
+  `broj_knjizice` int(20) NOT NULL,
   `ime_prezime` varchar(20) NOT NULL,
   `razred` varchar(20) NOT NULL,
   `odeljenje` varchar(20) NOT NULL
@@ -40,8 +40,8 @@ CREATE TABLE `djaci` (
 --
 
 INSERT INTO `djaci` (`id`, `broj_knjizice`, `ime_prezime`, `razred`, `odeljenje`) VALUES
-(1, '14-2020', 'Janko Veselinovic', 'II', '4'),
-(2, '13-2020', 'Ranko Stojic', 'III', '5');
+(1, 14, 'Janko Veselinovic', 'II', '4'),
+(2, 13, 'Ranko Stojic', 'III', '5');
 
 -- --------------------------------------------------------
 
@@ -51,6 +51,8 @@ INSERT INTO `djaci` (`id`, `broj_knjizice`, `ime_prezime`, `razred`, `odeljenje`
 
 CREATE TABLE `ocene` (
   `id` int(11) NOT NULL,
+  `id_predmeti` int(11) NOT NULL,
+  `id_broj_knjizice` int(11) NOT NULL,
   `ocene` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -58,11 +60,11 @@ CREATE TABLE `ocene` (
 -- Dumping data for table `ocene`
 --
 
-INSERT INTO `ocene` (`id`, `ocene`) VALUES
-(1, 4),
-(2, 5),
-(3, 2),
-(4, 3);
+INSERT INTO `ocene` (`id`, `id_predmeti`, `id_broj_knjizice`, `ocene`) VALUES
+(0, 1, 1, 5),
+(0, 1, 1, 5),
+(0, 1, 2, 2),
+(0, 3, 2, 3);
 
 -- --------------------------------------------------------
 
@@ -72,21 +74,18 @@ INSERT INTO `ocene` (`id`, `ocene`) VALUES
 
 CREATE TABLE `predmeti` (
   `id` int(11) NOT NULL,
-  `id_djaci` int(11) NOT NULL,
   `naziv` varchar(20) NOT NULL,
-  `profesor` varchar(20) NOT NULL,
-  `id_ocene` int(11) NOT NULL
+  `profesor` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `predmeti`
 --
 
-INSERT INTO `predmeti` (`id`, `id_djaci`, `naziv`, `profesor`, `id_ocene`) VALUES
-(1, 1, 'srpskog', 'SR', 1),
-(2, 1, 'matematika', 'MT', 2),
-(3, 2, 'matematika', 'MT', 3),
-(4, 2, 'srpski', 'FF', 4);
+INSERT INTO `predmeti` (`id`, `naziv`, `profesor`) VALUES
+(1, 'matematika', 'MT'),
+(2, 'srpski', 'SR'),
+(3, 'srpski', 'FF');
 
 --
 -- Indexes for dumped tables
@@ -102,15 +101,14 @@ ALTER TABLE `djaci`
 -- Indexes for table `ocene`
 --
 ALTER TABLE `ocene`
-  ADD PRIMARY KEY (`id`);
+  ADD KEY `id_predmeti` (`id_predmeti`),
+  ADD KEY `id_broj_knjizice` (`id_broj_knjizice`);
 
 --
 -- Indexes for table `predmeti`
 --
 ALTER TABLE `predmeti`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_djaci` (`id_djaci`),
-  ADD KEY `id_ocene` (`id_ocene`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -123,27 +121,21 @@ ALTER TABLE `djaci`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `ocene`
---
-ALTER TABLE `ocene`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
 -- AUTO_INCREMENT for table `predmeti`
 --
 ALTER TABLE `predmeti`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `predmeti`
+-- Constraints for table `ocene`
 --
-ALTER TABLE `predmeti`
-  ADD CONSTRAINT `predmeti_ibfk_1` FOREIGN KEY (`id_djaci`) REFERENCES `djaci` (`id`),
-  ADD CONSTRAINT `predmeti_ibfk_2` FOREIGN KEY (`id_ocene`) REFERENCES `ocene` (`id`);
+ALTER TABLE `ocene`
+  ADD CONSTRAINT `ocene_ibfk_1` FOREIGN KEY (`id_predmeti`) REFERENCES `predmeti` (`id`),
+  ADD CONSTRAINT `ocene_ibfk_2` FOREIGN KEY (`id_broj_knjizice`) REFERENCES `djaci` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
